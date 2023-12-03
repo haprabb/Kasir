4ea8b2d02d41a5d252da58f63483e3ea757f8165
Rizqi Satya Haprabu <112357810+haprabb@users.noreply.github.com>
2023-12-03 13:57:49 +0700

    kelas koneksi

---
+++ src/adminkasir/koneksiDatabase.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adminkasir;
+
+import java.sql.*;
+
+/**
+ *
+ * @author User
+ */
+public class koneksiDatabase {
+     private static koneksiDatabase instance;
+    private Connection koneksi;
+   
+    private koneksiDatabase(){
+        String url= "jdbc:mysql://localhost:3306/databasekasir";
+        String username = "root";
+        String password = "";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.koneksi = DriverManager.getConnection(url, username, password);
+        }catch (ClassNotFoundException | SQLException ex){
+            System.out.println(ex.getMessage());
+         }
+    }
+    
+    public static koneksiDatabase getInstance(){
+        if(instance == null){
+            instance = new koneksiDatabase();
+        }
+        return instance;
+    }
+    
+    public Connection getConnection(){
+        return koneksi;
+    }
+}
