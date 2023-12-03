1ac9bce57cc5eed2f4820624460b85dd6e5c871c
tbhan <tbhan@192.168.56.1>
2023-12-03 21:20:58 +0700

    test push

--- src/adminkasir/AdminKasir.java
+++ src/adminkasir/AdminKasir.java
@@ -14,7 +14,7 @@ public class AdminKasir {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello worlds");
+        System.out.println("hello world");
     }
     
 }
