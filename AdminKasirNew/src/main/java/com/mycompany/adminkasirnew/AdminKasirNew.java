243cebf99738e4c58241e30cc10a657f5f053e63
Rizqi Satya Haprabu <112357810+haprabb@users.noreply.github.com>
2023-12-13 22:42:52 +0700

    Add files via upload

---
+++ AdminKasirNew/src/main/java/com/mycompany/adminkasirnew/AdminKasirNew.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adminkasirnew;
+
+/**
+ *
+ * @author tbhan
+ */
+public class AdminKasirNew {
+
+    public static void main(String[] args) {
+        
+    }
+}
